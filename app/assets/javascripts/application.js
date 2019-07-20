b437f1029581e23e76ac3b3ed9b6ba72e2ddd70f
nguyenduong2612 <nguyenduong2612@gmail.com>
2019-07-20 14:22:27 +0700

    add login, logout, signup

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,6 @@
+//= require jquery3
+//= require jquery_ujs
+//= require bootstrap
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  resources :microposts
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  get '/logout',  to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
